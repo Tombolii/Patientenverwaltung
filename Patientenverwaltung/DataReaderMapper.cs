14d52d796a9fdd9db87621dc24b96f6497df64be
tom.wazlawik <tom.wazlawik@itSchuleStuttgart1.onmicrosoft.com>
2022-12-15 11:30:03 +0100

    Add DataReaderMapper and DBAdapter

---
+++ Patientenverwaltung/DataReaderMapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Patientenverwaltung.Model;
+
+namespace Patientenverwaltung
+{
+    internal class DataReaderMapper
+    {
+
+        public Patient extractPatientFromReader(MySqlDataReader reader)
+        {
+            Patient patient = new Patient();
+            while (reader.Read())
+            {
+                patient.idPatient = reader.GetInt32("idPatient");
+                patient.versicherungsnummer = reader.GetInt32("versicherungsnummer");
+                patient.versicherung = reader.GetString("name");
+                extractPersonendatenFromReader(reader, patient);
+            }
+            return patient;
+        }
+
+        private void extractPersonendatenFromReader(MySqlDataReader reader, Patient patient)
+        {
+            while (reader.Read())
+            {
+                patient.vorname = reader.GetString("vorname");
+                patient.nachname = reader.GetString("nachname");
+                patient.email = reader.GetString("email");
+                patient.telefonnummer = reader.GetInt32("telefonnummer");
+                patient.geburtstag = DateTime.Parse(reader.GetString("geburtstag"));
+                Adresse adresse = new Adresse();
+                adresse.straße = reader.GetString("straße");
+                adresse.hausnummer = reader.GetString("hausnummer");
+                adresse.ort = reader.GetString("ort");
+                adresse.plz = reader.GetInt32("plz");
+                patient.adresse = adresse;
+            }
+        }
+    }
+}
