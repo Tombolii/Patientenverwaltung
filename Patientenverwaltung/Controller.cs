90078474013fe897a164415c0f251ea0a6435e58
samja02 <jaschke.samuel13@gmail.com>
2023-01-10 09:05:44 +0100

    Get-Methoden im Controller

--- Patientenverwaltung/Controller.cs
+++ Patientenverwaltung/Controller.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Patientenverwaltung;
+using Patientenverwaltung.Model;
+using Patientenverwaltung.Datenbank;
 using Patientenverwaltung.Gui;
-
+using Bericht = Patientenverwaltung.Gui.Bericht;
 
 namespace Patientenverwaltung
 {
+    
     class Controller
     {
+        
         Bericht bericht = new Bericht();
         BerichtBearbeiten berichtBearbeiten = new BerichtBearbeiten();
         BerichtDaten_cs berichtDaten = new BerichtDaten_cs();
@@ -22,8 +26,28 @@ namespace Patientenverwaltung
         PatientenDaten patientenDaten = new PatientenDaten();
         TerminBearbeiten terminBearbeiten = new TerminBearbeiten ();
         TerminDaten terminDaten = new TerminDaten ();
+        DBAdapter adapter = new DBAdapter();
 
+        public List<Termin> GetTerminArzt(int idArzt)
+        {
+            return adapter.getTermineOfArzt(idArzt);            
 
+        }
+        public List<Termin> GetTerminPatient(int idPatient)
+        {
+            return adapter.getTermineOfPatient(idPatient);
+        }
+
+        public List<Bericht> GetBericht(int idPatient)
+        {
+            return null;
+        }
+
+        public List<Krankheitsbild> GetKrankheiten(int idPatient)
+        {
+            return null;
+        }
+        
 
     }
 }
