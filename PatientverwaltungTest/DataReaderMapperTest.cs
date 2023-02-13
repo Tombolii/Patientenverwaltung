5611257b6ad8e38937b2f92c4bfb9ac9bf0a06a5
tom.wazlawik <tom.wazlawik@itSchuleStuttgart1.onmicrosoft.com>
2023-02-13 12:13:29 +0100

    add unit tests initial

---
+++ PatientverwaltungTest/DataReaderMapperTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MySql.Data.MySqlClient;
+using Moq;
+using Patientenverwaltung;
+using Patientenverwaltung.Model;
+
+
+namespace PatientverwaltungTest
+{
+    public class DataReaderMapperTest
+    {
+
+        DataReaderMapper underTest = new DataReaderMapper();
+
+        [Test]
+        public void Test1()
+        {
+            // given
+            int expectedIdPatient = 1;
+            Mock<> reader = new Mock<MySqlDataReader>();
+            reader.SetupGet(x => x.GetInt32("idPatient")).Returns(expectedIdPatient);
+
+            //when
+            List<Patient> result = underTest.extractPatientenFromReader(reader.Object);
+            Assert.Equals(expectedIdPatient, result[0].idPatient);
+        }
+    }
+}
